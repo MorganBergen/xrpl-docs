f91bcebbe7a39f2ddb0cd0a8c9ca0a36db9c4e11
morgan bergen <morganmahabergen@icloud.com>
2024-02-12 16:03:56 -0600

    updated console log message

--- 03-ledger-data/main.ts
+++ 03-ledger-data/main.ts
@@ -2,7 +2,7 @@ export const serverInfo = async () => {
     const client = new Client("wss://s.altnet.rippletest.net:51233")
     await client.connect()
     if (client.isConnected()){
-        console.log("testing commit")
+        console.log("testing if connected")
         clinet.disconnected;
     }
 }
